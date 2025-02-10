0b4f4c5212f71c182fa330f40dfa5cfd3643f0c8
IvanB04 <ivanbustamante319@gmail.com>
2025-02-10 15:39:48 -0700

    TESTING 1ST PUSH

---
+++ src/main/java/asu/asu/AsuApplication.java
@@ -0,0 +1,13 @@
+package asu.asu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/asu/asu/AsuApplicationTests.java
@@ -0,0 +1,13 @@
+package asu.asu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
